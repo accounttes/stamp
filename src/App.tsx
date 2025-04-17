2a126a6e40ccdf1b4d9d04feb7a47b311f41b579
robert <smaleskirobert@gmail.com>
2025-04-17 14:42:32 +0500

    fix: import react deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import { ProductSelection } from './components/ProductSelection';
